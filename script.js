1dfda8b490ece3a411f8229d9d1ada1434d34580
NatalyaBolshakova <61007035+NatalyaBolshakova@users.noreply.github.com>
2020-03-18 00:28:29 +0400

    feat: implement portfolio action

--- script.js
+++ script.js
@@ -1,26 +1,50 @@
 let itemMenu = document.querySelector("body > header > nav > ul");
+let btnNav = document.querySelector("body > main > section.portfolio > .container_button");
+let Picture = document.querySelector("body > main > section.portfolio > .container_portfolio");
+//document.querySelector("body > main > section.portfolio > div")
 let step = 0;
 
 
 const selectItem = (event) => {
     step++;
     
-    // click - change color text
-    //console.log(`step ${step}`, event.target);
-
-    if (event.target.closest('li').classList[0] != 'active_li' & event.target.tagName === 'A') {
+    // click - change active links of header
+    if (/*event.target.closest('li').classList[0] != 'active_li' & */event.target.tagName === 'A') {
         document.querySelectorAll('li').forEach(li => li.classList.remove('active_li'));
         event.target.closest('li').classList.add('active_li');         
     }    
 }
+
+const selectButton = (event) => {
+    step++;
+    // click - change the active portfolio button and image rotation position
+    
+   if (/*event.target.classList[1] != 'button_active' & */event.target.tagName === 'BUTTON') {
+        document.querySelectorAll('.container_button>button').forEach(button => button.classList.remove('button_active'));
+        event.target.classList.add('button_active');
+
+        document.querySelectorAll('.container_portfolio>img').forEach(img => img.style.order = Math.floor(Math.random() * document.querySelectorAll('.container_portfolio>img').length));         
+    }
+}
+
+const activePicture = (event) => {        
+    if (event.target.tagName === 'IMG'){
+        document.querySelectorAll('.container_portfolio>img').forEach(img => img.classList.remove('img_active'));
+        event.target.classList.add('img_active');
+    };    
+}
+
    
 itemMenu.addEventListener('click', selectItem);
+btnNav.addEventListener('click', selectButton);
+Picture.addEventListener('click', activePicture);
+
 
 
 /*const linkHash = (event) =>{
     step++;
     console.log(`step ${step}`, event.target);
-    /*console.log(`step ${step}`, event.currentTarget);
+    console.log(`step ${step}`, event.currentTarget);
    console.log(`step ${step}`, event.target.parentElement);
    console.log(`step ${step}`, event.currentTarget.parentElement);
    console.log(event.target.children[0]);
